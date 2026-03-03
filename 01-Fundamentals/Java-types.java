da994beef2d7606ad02c8c0265b28f378423d4ee
Omar Carrillo <124636738+OmarCarrillo1121@users.noreply.github.com>
2026-03-03 16:56:55 -0600

    Document Java variable types and conventions
    Added comments explaining Java variable declaration, primitive types, naming conventions, and text blocks.

---
+++ 01-Fundamentals/Java-types.java
@@ -0,0 +1,26 @@
+//VARIABLES
+// To declare a variable, first define vraiable type, then variable name and 
+// you can initialize with a value or just declare it
+
+// PRIMITIVES
+// There are 8 types of primitives in Java: 
+// boolean: true or false
+// byte: 8 bit integer
+// char: unicode characters
+// short: 16 bit integer
+// int: 32 bit integer
+// long: 64 bit integer
+// float: 32 bit floating number up to 7 digits
+// double: 64 bit floating number up to 15 digits
+
+//NAMING CONVENTIONS
+// Variables - camelCase
+// Methods - camelCase
+// Constant - UPPERCASE
+// Classes - PascalCase
+// Use spaces to separate operators, keywords and control flow statements
+// if (condition) {
+
+// TEXT BLOCK
+// Multiline text introduced in Java 15
+// Use """ before and after text """
